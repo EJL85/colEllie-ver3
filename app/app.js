49f386fe3aca4822c082d21dfc434e88ef6f20ff
Coleman Imhoff <colemanimhoff@gmail.com>
2018-05-16 14:16:54 -0600

    added app.js with classMates array

---
+++ app/app.js
@@ -0,0 +1,27 @@
+let classMates = [
+    {
+        name: 'Coleman',
+        favoriteLanguage: 'React',
+        hobby: Music
+    },
+    {
+        name: 'Ellie',
+        favoriteLanguage: 'JavaScript',
+        hobby: 'Speed Running'
+    },
+    {
+        name: 'Andrew',
+        favoriteLanguage: 'CSS',
+        hobby: 'DnD'
+    },
+    {
+        name: 'Alyssa',
+        favoriteLanguage: 'Perl',
+        hobby: 'Comics'
+    },
+    {
+        name: 'Keith',
+        favoriteLanguage: 'Block Chain',
+        hobby: 'Drinkin Beerz'
+    },
+]
